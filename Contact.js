f4bffd208a984d6180e8416e938b897fc87538ea
namitha-santhosh <namitha.santhosh@people10.com>
2023-08-22 15:06:31 +0530

    HTML CSS Basics

---
+++ Contact.js
@@ -0,0 +1,28 @@
+// Contact.js
+
+export default class Contact {
+
+    constructor(form){
+        this.fullName = form.elements["fullName"].value;
+        this.email = form.elements["email"].value;
+        this.subject = form.elements["subject"].value;
+        this.body = form.elements["msg"].value;
+    }
+    
+    fullName = "";
+    email = "";
+    subject = "";
+    body = "";
+
+    send() {
+        console.info(this.formatMessage());
+        document.getElementById("formInfo").innerHTML = "We're not sending emails yet....next update";
+    }
+
+    formatMessage() {
+        return `To: ${this.fullName}
+                Email: ${this.email}
+                Subject: ${this.subject}
+                Body: ${this.body}`;
+    }
+};
---
+++ index.js
@@ -0,0 +1,47 @@
+// var number = 5;
+// let name = "Namitha";
+
+// document.writeln(name+number);
+// // alert("Hello World");
+
+import Contact from "./Contact.js";
+
+
+const formInfo = document.getElementById("formInfo");
+
+let hasJob = true;
+if(hasJob){
+    showMessage("Thanks for visiting,I'm currently employed.");
+} else{
+    showMessage("Please look around, let me know of new opportunities!");
+}
+
+let today=new Date("08/21/2023");
+let dayOfWeek = today.getDay();
+if(dayOfWeek === 0 || dayOfWeek ===6){
+    showMessage("Since it's a weekend, please be patient.")
+}
+
+function showMessage(message) {
+    formInfo.innerHTML= "<p>"+ message +"</p>";
+}
+
+const contactForm = document.getElementById("contactForm");
+contactForm.addEventListener("submit",function(event) {
+    event.preventDefault();
+    const contact = new Contact(contactForm);
+    contact.send();
+    showMessage("Sending your msg...Thank you:" + contact.fullName);
+});
+
+const experiences = document.getElementsByClassName("experience");
+for (let x = 0; x < experiences.length; x++){
+    const item = experiences[x];
+    item.addEventListener("mouseenter", function(event) {
+        event.target.style = "background-color: #999999";
+    });
+    item.addEventListener("mouseleave", function(event) {
+        event.target.style = "";
+    });
+}
+
